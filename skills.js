a0c77040b82f0beaf5471cbf765b5517e39574bc
Madelyn Ore <madelynore@gmail.com>
2024-01-03 15:43:45 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,5 @@
+function calculateNumbers(var1, var2) {     
+    var firstNumber = document.getElementById('firstNumber').value;
+    var secondNumber = document.getElementById('secondNumber').value;
+    var result = firstNumber * secondNumber;
+    document.getElementById('result').value = result}
